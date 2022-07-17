17b300605de58a1e4ee5645883bf6c6b8f275dc9
Iribeiro <iribeiro@kryptontech.com.br>
2022-07-17 03:00:55 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>DSMeta</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://www.linkedin.com/in/igor-ribeiro-a1a670174/"> Igor Ribeiro</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
